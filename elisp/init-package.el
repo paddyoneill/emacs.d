7a0b1c347860caf86244dd3612e16ba298f653e0
Patrick O'Neill <paddy.oneill93@gmail.com>
2021-12-18 11:48:09 +0000

    Update init-package.el
    * Install use-package

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -3,13 +3,27 @@
 (require 'package)
 
 ;;; Package repositories
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;;; Initialise package.el
-
 (package-initialize)
 
+;;; Install use-package if it is not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; Setup use-package
+(eval-and-compile
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t))
+
+(eval-when-compile
+  (require 'use-package))
+
+;;; Install requirements for use-package
+(use-package diminish)
+(use-package bind-key)
 
 (provide 'init-package)
 ;;; init-package.el ends here
